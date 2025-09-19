a55b048a0d8bb25de5cba35ca4745420ac4b80a8
Oliko136 <olchick136@gmail.com>
2025-09-19 16:19:10 +0400

    Clear all from local storage

--- js/app.js
+++ js/app.js
@@ -11,6 +11,8 @@ class CalorieTracker {
         this._displayCaloriesBurned();
         this._displayCaloriesRemaining();
         this._displayCaloriesProgress();
+
+        document.getElementById('limit').value = this._calorieLimit;
     }
 
     // Public methods (API) //
@@ -63,6 +65,7 @@ class CalorieTracker {
         this._totalCalories = 0;
         this._meals = [];
         this._workouts = [];
+        Storage.clearAll();
         this._renderStats();
     }
 
@@ -282,6 +285,12 @@ class Storage {
         })
 
         localStorage.setItem('workouts', JSON.stringify(workouts));
+    }
+
+    static clearAll() {
+        localStorage.removeItem('totalCalories');
+        localStorage.removeItem('meals');
+        localStorage.removeItem('workouts');
     }
 }
 
